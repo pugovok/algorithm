5ffb888249b073fcea7d0a519090779a8c9d93f3
Максим Купцов <kuptsov@sports.ru>
2018-11-05 15:12:45 +0300

    task: House of Pancakes

--- helpers/read_example.go
+++ helpers/read_example.go
@@ -1,1 +1,34 @@
 package helpers
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+var scanner *bufio.Scanner = bufio.NewScanner(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func ReadInt32() int {
+	scanner.Scan()
+	ans, _ := strconv.Atoi(scanner.Text())
+	return ans
+}
+
+func PrintInts(ints ...int) {
+	for _, value := range ints {
+		writer.WriteString(strconv.Itoa(value))
+		writer.WriteByte(' ')
+	}
+}
+
+func main() {
+	defer writer.Flush() // очищает буфер при выходе из функции main
+	scanner.Split(bufio.ScanWords) // заставляет scanner считать отдельными токенами слова, разделенные пробелами
+	// также имеются функции bufio.ScanLines и bufio.ScanBytes
+	var n, m int = ReadInt32(), ReadInt32()
+	PrintInts(n, m)
+	writer.WriteByte('\n')
+	slice := []int{1, 2, 3, 4, 5}
+	PrintInts(slice...)
+}
--- tasks/codejam2015_b/pancakes.go
+++ tasks/codejam2015_b/pancakes.go
@@ -3,6 +3,69 @@
 //   tags: greedy
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type scanner struct {
+	s *bufio.Scanner
+}
+
+func initScanner(filename string) scanner {
+	f, _ := os.Open(filename)
+	return scanner{s: bufio.NewScanner(f)}
+}
+
+func (s scanner) ReadInt() int {
+	s.s.Scan()
+	val, _ := strconv.Atoi(s.s.Text())
+	return val
+}
+
 func main() {
+	fo, _ := os.Create("output.txt")
+	writer := bufio.NewWriter(fo)
+	defer fo.Close()
+
+	defer writer.Flush()
+
+	//scanner := initScanner("B-small-practice.in")
+	scanner := initScanner("B-large-practice.in")
+	//scanner := initScanner("input.txt")
+	scanner.s.Split(bufio.ScanWords)
+
+	countTests := scanner.ReadInt()
+
+	for currentTest := 0; currentTest < countTests; currentTest++ {
+		plates := scanner.ReadInt()
+
+		var p []int
+		maxPanCake := 0
+		for i := 0; i < plates; i++ {
+			v := scanner.ReadInt()
+			if v > maxPanCake {
+				maxPanCake = v
+			}
+			p = append(p, v)
+		}
+
+		ret := maxPanCake
+		for x := 1; x < maxPanCake; {
+
+			moves := 0
+			for _, Pi := range p {
+				moves += (Pi - 1) / x
+			}
+
+			if ret > moves+x {
+				ret = moves + x
+			}
+			x++
+		}
+		writer.WriteString(fmt.Sprintf("Case #%d: %d\n", currentTest+1, ret))
+	}
 
 }
